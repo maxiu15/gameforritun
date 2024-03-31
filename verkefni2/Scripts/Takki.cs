bec15ef33e1f910f9878b4d748fe2e3b68691f57
maxiu15 <111794942+maxiu15@users.noreply.github.com>
2024-03-31 12:56:46 +0000

    Add files via upload

---
+++ verkefni2/Scripts/Takki.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Takki : MonoBehaviour
+{
+    public TextMeshProUGUI texti;
+   
+    public void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex==3) // við erum að búa til hnapp fyrir lokaatriðin
+        {
+            texti.text = "Lokastig " + PlayerMovment.count.ToString();
+        }
+        
+    }
+    public void Byrja() // við hleðum 1. lvl
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Endir() // við hleðum upphafssenuna
+    {
+        SceneManager.LoadScene(0);
+        PlayerMovment.count = 0;
+    }
+    
+}
---
+++ verkefni2/Scripts/hlutursnu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hlutursnu : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,80,0) * Time.deltaTime); // við látum peninga snúast.
+    }
+}
